78c492a890418d842b58f8eee7be33b8ca92eb8d
29Tatiana <amiravob@mail.ru>
2022-09-14 14:26:11 +0300

    задача29r

---
+++ задача29/Program.cs
@@ -0,0 +1,13 @@
+﻿// Пользователь вводит 8 чисел. Поместите их в массив и распечатайте.
+Console.WriteLine("Введите 8 чисел через пробел");
+string numbers = Console.ReadLine()!;
+string[] parts = numbers.Split(" ");
+int[] array = new int[8];
+
+for (int i = 0; i < 8; i++)
+{
+    array[i] = int.Parse(parts[i]);
+}
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
