f219ab537328e331b4bd4bb02fd82c1105a7e104
29Tatiana <amiravob@mail.ru>
2022-09-14 12:14:31 +0300

    задача25

--- задача25/Program.cs
+++ задача25/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.Write("Введите числа А и В:");
+string AB = Console.ReadLine()!;
+string[] AB = AB.Split(' ');
+int x1 = int.Parse(parts_A[0]);
+int y1 = int.Parse(parts_A[1]);
