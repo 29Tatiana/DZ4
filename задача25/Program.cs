3b9d89b7fff1324809f0f77d32c2a2ee18e0f16d
29Tatiana <amiravob@mail.ru>
2022-09-14 14:25:33 +0300

    задача25r

--- задача25/Program.cs
+++ задача25/Program.cs
@@ -1,6 +1,14 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-Console.Write("Введите числа А и В:");
+Console.Write("Введите числа А и В через пробел:");
 string AB = Console.ReadLine()!;
-string[] AB = AB.Split(' ');
-int x1 = int.Parse(parts_A[0]);
-int y1 = int.Parse(parts_A[1]);
+string[] parts_AB = AB.Split(' ');
+int numA = int.Parse(parts_AB[0]);
+int numB = int.Parse(parts_AB[1]);
+
+double result = 1;
+for (int i = 0; i < numB; i++)
+{
+    result *= numA;
+}
+
+Console.WriteLine($"A в степени В = {result}");
